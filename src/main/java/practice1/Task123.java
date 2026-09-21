1f459f85ce10047fff3de952ce03d26cd07e4390
hopesofbuzzy <nurbaevdaniar0@gmail.com>
2026-09-21 17:37:09 +0500

    рПраÑкиитика 1

--- src/main/java/practice1/Task123.java
+++ src/main/java/practice1/Task123.java
@@ -1,4 +1,4 @@
-package org.example;
+package practice1;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
--- src/main/java/practice1/Task4.java
+++ src/main/java/practice1/Task4.java
@@ -1,4 +1,4 @@
-package org.example;
+package practice1;
 import java.util.Scanner;
 
 
